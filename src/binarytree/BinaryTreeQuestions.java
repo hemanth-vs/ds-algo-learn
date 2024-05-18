a5ce3d405251e5ad00f595755c65c0d8335fb73e
hemanth-vs <s.hemanthvarma@gmail.com>
2024-05-17 19:27:22 -0700

    add source, copyright information

--- src/binarytree/BinaryTreeQuestions.java
+++ src/binarytree/BinaryTreeQuestions.java
@@ -2,7 +2,8 @@ package binarytree;
 /*
  * Source: http://cslibrary.stanford.edu/110/BinaryTrees.html
  * 
- * This is article #110 in the Stanford CS Education Library. This and other free CS materials are available at the library (http://cslibrary.stanford.edu/). That people seeking education should have the opportunity to find it. This article may be used, reproduced, excerpted, or sold so long as this paragraph is clearly reproduced. Copyright 2000-2001, Nick Parlante, nick.parlante@cs.stanford.edu. 
+ * 
+ This is article #110 in the Stanford CS Education Library. This and other free CS materials are available at the library (http://cslibrary.stanford.edu/). That people seeking education should have the opportunity to find it. This article may be used, reproduced, excerpted, or sold so long as this paragraph is clearly reproduced. Copyright 2000-2001, Nick Parlante, nick.parlante@cs.stanford.edu. 
  */
 public class BinaryTreeQuestions extends BinaryTree{
     
